a469865542b60f28687c4738dda10062264ea320
Pedro Colletti Silva <pedrocolletti@icloud.com>
2023-08-20 13:06:21 -0300

    Primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>asdasdasdasdaddaas</h1>
+      <h1>Teste</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
