9b67aba8f73fb7d5b3e04814704a396d35122431
Pedro Colletti Silva <pedrocolletti@icloud.com>
2023-08-21 17:25:37 -0300

    final adjusts

--- src/scenes/Game/Game.tsx
+++ src/scenes/Game/Game.tsx
@@ -2,26 +2,65 @@ import { If } from "../../helpers";
 import "./Game.css";
 import clouds from "/clouds.png";
 import mario from "/mario.gif";
+import marioStop from "/mario-stop.gif";
 import pipe from "/pipe.png";
+import floor from "/floor.png";
+import gameOver from "/game-over.png";
 
 type Props = {
+  count: number;
   start: boolean;
 };
 
-const Game: React.FC<Props> = ({ start }) => {
+const Game: React.FC<Props> = ({ count, start }) => {
   return (
     <>
+      <div className="game-board">
+        <img id="clouds" className="clouds" src={clouds} alt="clouds" />
+        <If hasCondition={start}>
+          <>
+            <img className="pipe" src={pipe} alt="pipe" />
+            <img className="mario" src={mario} alt="mario" />
+          </>
+        </If>
+        <If hasCondition={!start && count === 0}>
+          <>
+            <img className="mario-stop" src={marioStop} alt="mario" />
+          </>
+        </If>
+        <If hasCondition={!start && count === 1}>
+          <>
+            <img className="pipe-position" src={pipe} alt="pipe" />
+            <img className="game-over" src={gameOver} alt="gameOver" />
+          </>
+        </If>
+      </div>
       <If hasCondition={start}>
         <>
-          <button id="reloadButton">Quer tentar novamente ?</button>
+          <img className="floor" src={floor} alt="floor" />
+          <div id="reloadButton" className="madeBy">
+            {" "}
+            Feito por Pedro Colletti 👋
+          </div>
         </>
       </If>
-      <div className="game-board">
-        <img className="clouds" src={clouds} alt="clouds" />
-        <img className="mario" src={mario} alt="mario" />
-        <img className="pipe" src={pipe} alt="pipe" />
-      </div>
-      <div className="madeBy"> Feito por Pedro Colletti 👋</div>
+      <If hasCondition={!start && count === 0}>
+        <>
+          <img className="floor" src={floor} alt="floor" />
+          <div id="reloadButton" className="madeBy">
+            aperte ESPAÇO para jogar
+          </div>
+        </>
+      </If>
+      <If hasCondition={!start && count === 1}>
+        <>
+          <img className="floor" src={floor} alt="floor" />
+
+          <div id="reloadButton" className="madeBy">
+            aperte ESPAÇO recomeçar
+          </div>
+        </>
+      </If>
     </>
   );
 };
--- src/scenes/Game/index.tsx
+++ src/scenes/Game/index.tsx
@@ -3,67 +3,78 @@ import Game from "./Game";
 
 const GameContainer: React.FC = () => {
   const [start, setStart] = useState(false);
+  const [count, setCount] = useState(0);
   const mario = document.querySelector(".mario");
   const pipe = document.querySelector(".pipe") as HTMLElement;
   const marioElements = document.querySelector(".mario") as HTMLElement;
-  const imgElement = document.querySelector(".mario") as HTMLImageElement;
-  const clouds = document.querySelector(".clouds") as HTMLElement;
-
-  const jump = () => {
-    mario?.classList.add("jump");
-
-    setTimeout(() => {
-      mario?.classList.remove("jump");
-    }, 750);
-  };
-
-  const loop = setInterval(() => {
-    if (mario) {
-      const pipePosition = pipe?.offsetLeft;
-
-      const cloudPosition = +window
-        .getComputedStyle(clouds)
-        .left.replace("px", "");
-
-      const marioPosition = +window
-        .getComputedStyle(mario)
-        .bottom.replace("px", "");
-
-      if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-        pipe.style.animation = "none";
-        pipe.style.left = `${pipePosition}px`;
-
-        clouds.style.animation = "none";
-        clouds.style.left = `${cloudPosition}px`;
-
-        marioElements.style.animation = "none";
-        marioElements.style.bottom = `${marioPosition}px`;
-
-        imgElement.src = "./game-over.png";
-        marioElements.style.width = "70px";
-        marioElements.style.marginLeft = "50px";
-        marioElements.style.zIndex = "9999";
-
-        setStart(true);
-        clearInterval(loop);
-      }
-    }
-  }, 10);
-
-  document.addEventListener("keydown", function (event) {
-    if (event.code === "Space") {
-      jump();
-    }
-  });
-
-  console.log("start", start);
-
   const reloadButton = document.getElementById("reloadButton");
 
-  reloadButton?.addEventListener("click", function () {
-    location.reload();
+  reloadButton?.addEventListener("keydown", function (event) {
+    if (event.code === "Space") {
+      setStart(true);
+    }
   });
 
-  return <Game start={start} />;
+  const windowWidth = window.innerWidth;
+  const value = windowWidth > 900 ? 750 : 1750;
+
+  if (!start) {
+    const jump = () => {
+      setStart(true);
+      mario?.classList.add("jump");
+
+      setTimeout(() => {
+        mario?.classList.remove("jump");
+      }, value);
+    };
+
+    document.addEventListener("keydown", function (event) {
+      if (event.code === "Space") {
+        setStart(true);
+        setStart(true);
+        jump();
+      }
+    });
+  } else {
+    const jump = () => {
+      setStart(true);
+      mario?.classList.add("jump");
+
+      setTimeout(() => {
+        mario?.classList.remove("jump");
+      }, value);
+    };
+
+    document.addEventListener("keydown", function (event) {
+      if (event.code === "Space") {
+        setStart(true);
+        jump();
+      }
+    });
+
+    const loop = setInterval(() => {
+      if (mario) {
+        const pipePosition = pipe?.offsetLeft;
+
+        const marioPosition = +window
+          .getComputedStyle(mario)
+          .bottom.replace("px", "");
+
+        if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+          pipe.style.animation = "none";
+          pipe.style.left = `${pipePosition}px`;
+
+          marioElements.style.animation = "none";
+          marioElements.style.bottom = `${marioPosition}px`;
+
+          setStart(false);
+          setCount(+1);
+
+          clearInterval(loop);
+        }
+      }
+    }, 10);
+  }
+  return <Game start={start} count={count} />;
 };
 export default GameContainer;
